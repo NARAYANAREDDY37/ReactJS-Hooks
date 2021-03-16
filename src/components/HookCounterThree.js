2309a5777c3f772b1e0601032fb1b9f508fa40bc
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-03-16 18:15:53 +0530

    added Hooks with object

---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+function HookCounterThree() {
+
+  let [count, setName] = useState({})
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookCounterThree
