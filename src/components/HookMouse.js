455ab620277520339b7c9fd37a05bf2446eb3bff
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-03-17 11:00:00 +0530

    added useEffect with cleanup

---
+++ src/components/HookMouse.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+
+//same is implemented with useEffect in functional component
+//compare ClassMouse vs HookMouse
+
+function HookMouse() {
+
+  //creating state variables for X and Y initialized to zero 
+  let [x, setX] = useState(0)
+  let [y, setY] = useState(0)
+
+  //now we should addEventListener for mouse event, and this is where we use effect Hook
+
+  //logMousePosition = Event Handler
+  let logMousePosition = e => {
+    console.log("Mouse event")
+    setX(e.clientX)
+    setY(e.clientY)
+  }
+
+
+  useEffect(() => {
+    console.log('useEffect called')
+    window.addEventListener('mousemove', logMousePosition)
+  }, []) //we specify empty array not to call useEffect on every re-render of component, the same thing is done in class component using componentDidMount
+
+  //so we mimiced the componentDidMount in class component using empty array
+
+  return (
+    <div>
+      Hooks X- {x} and Y- {y}
+    </div>
+  )
+}
+
+export default HookMouse
